79f42272046a0ad68af482f7ee37b1c070104a2e
Donald Brooks <brooksd079@gmail.com>
2023-06-07 09:06:46 +0300

    Word replacemnt program.

--- Word Replacement/app.py
+++ Word Replacement/app.py
@@ -2,4 +2,7 @@ def replace_word():
     
     str = 'Hello, Im revisiting Python fundamentals'
     word_to_replace = input('Enter the Word to Replace:')
-    word_replacement = input('Enter the Word Replacement') 
+    word_replacement = input('Enter the Word Replacement:') 
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
