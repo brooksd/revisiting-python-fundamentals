07ed49896260a2aa053c3b1f9a42123ec4233632
Donald Brooks <brooksd079@gmail.com>
2023-06-07 09:04:17 +0300

    Word replacemnt program.

---
+++ Word Replacement/app.py
@@ -0,0 +1,5 @@
+def replace_word():
+    
+    str = 'Hello, Im revisiting Python fundamentals'
+    word_to_replace = input('Enter the Word to Replace:')
+    word_replacement = input('Enter the Word Replacement') 
