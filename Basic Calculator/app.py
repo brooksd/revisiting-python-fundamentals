4ed0e20fb01f86d7f2bed1f16f276b20f61c4e68
Donald Brooks <brooksd079@gmail.com>
2023-06-07 09:29:18 +0300

    Basic Calcuator

--- Basic Calculator/app.py
+++ Basic Calculator/app.py
@@ -13,35 +13,37 @@ def multiply(a, b):
 def divide(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))
-    
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
-print("E. Exit")
 
-choice = input("Input your choice: ")
 
-if choice == "a" or choice == "A":
-    print("Addition")
-    a = int(input("input first number:"))
-    b = int(input("input second number:"))
-    add( a, b)
-elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int(input("input first number:"))
-    b = int(input("input second number:"))
-    sub( a, b)
-elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int(input("input first number:"))
-    b = int(input("input second number:"))
-    multiply( a, b)
-elif choice == "d" or choice == "D":
-    print("Addition")
-    a = int(input("input first number:"))
-    b = int(input("input second number:"))
-    divide( a, b)
-elif choice == "e" or choice == "E":
-    print("Program ended")
-    quit()
+while True:    
+    print("A. Addition")
+    print("B. Subtraction")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit")
+
+    choice = input("Input your choice: ")
+
+    if choice == "a" or choice == "A":
+        print("Addition")
+        a = int(input("input first number:"))
+        b = int(input("input second number:"))
+        add( a, b)
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        a = int(input("input first number:"))
+        b = int(input("input second number:"))
+        sub( a, b)
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        a = int(input("input first number:"))
+        b = int(input("input second number:"))
+        multiply( a, b)
+    elif choice == "d" or choice == "D":
+        print("Addition")
+        a = int(input("input first number:"))
+        b = int(input("input second number:"))
+        divide( a, b)
+    elif choice == "e" or choice == "E":
+        print("Program ended")
+        quit()
