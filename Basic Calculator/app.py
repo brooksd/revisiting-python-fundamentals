043fff59e37d3574629279dde6154a60ede23b96
Donald Brooks <brooksd079@gmail.com>
2023-06-07 09:24:57 +0300

    Basic Calcuator

---
+++ Basic Calculator/app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+    
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+    
+def multiply(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer))
