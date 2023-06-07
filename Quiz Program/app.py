f8e7b3f804c7c0196f3345135e892bc4902e6e21
Donald Brooks <brooksd079@gmail.com>
2023-06-07 11:19:00 +0300

    Quiz Program

---
+++ Quiz Program/app.py
@@ -0,0 +1,26 @@
+quiz = {
+    "Q1": {
+        'question': 'What is the capital of France?',
+        'answer': 'Paris'       
+    },
+    "Q2": {
+        'question': 'What is the capital of Kenya?',
+        'answer': 'Nairobi'       
+    },
+    "Q3": {
+        'question': 'What is the capital of Uganda?',
+        'answer': 'Kampala'       
+    },
+    "Q4": {
+        'question': 'What is the capital of United Kingdom?',
+        'answer': 'London'       
+    },
+    "Q5": {
+        'question': 'What is the capital of South Africa?',
+        'answer': 'Johannesburg'       
+    },
+    "Q6": {
+        'question': 'What is the capital of Unoted States?',
+        'answer': 'Washington DC'       
+    }
+}
