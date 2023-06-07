a69e62ab90777035eea8a4d777c8ce364b22ee52
Donald Brooks <brooksd079@gmail.com>
2023-06-07 10:29:28 +0300

    Binary Search

--- Binary Search Algo/app.py
+++ Binary Search Algo/app.py
@@ -3,3 +3,14 @@ def binary_search(list, element):
     start = 0 
     end = len(list)
     steps = 0
+    
+    while( start <= end):
+        print("Step", steps, ":" , str(list[start:end+1]) )
+        
+        steps = steps + 1
+        middle = (start + end) / 2
+        
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle - 1
