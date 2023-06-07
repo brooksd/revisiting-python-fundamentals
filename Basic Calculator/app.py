d9fee61cdbf5546077d9b1477398a65c0bfa3e19
Donald Brooks <brooksd079@gmail.com>
2023-06-07 09:26:55 +0300

    Basic Calcuator

--- Basic Calculator/app.py
+++ Basic Calculator/app.py
@@ -24,23 +24,23 @@ choice = input("Input your choice: ")
 
 if choice == "a" or choice == "A":
     print("Addition")
-    a = int(input("input first number"))
-    b = int(input("input second number"))
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
     add( a, b)
 elif choice == "b" or choice == "B":
     print("Subtraction")
-    a = int(input("input first number"))
-    b = int(input("input second number"))
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
     sub( a, b)
 elif choice == "c" or choice == "C":
     print("Multiplication")
-    a = int(input("input first number"))
-    b = int(input("input second number"))
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
     multiply( a, b)
 elif choice == "d" or choice == "D":
     print("Addition")
-    a = int(input("input first number"))
-    b = int(input("input second number"))
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
     divide( a, b)
 elif choice == "e" or choice == "E":
     print("Program ended")
