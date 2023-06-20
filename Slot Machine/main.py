35a2dee35bba63cd1216fd4dc1fcafbd04bda6e3
Donald Brooks <brooksd079@gmail.com>
2023-06-20 18:28:13 +0300

    number of playable lines

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -1,3 +1,7 @@
+# Global Constant dictating the number wof lines in the slot machine
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -9,10 +13,27 @@ def deposit():
                 print("amount must be greter than $0.")
         else:
             print("Please enter a Number.")
-            
-    return amount    
-        
+
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines= input("Enter the number of line to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a Number.")
+
+    return lines
+
 def main():
-    balance = deposit()       
-    
-main() 
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
