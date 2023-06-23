01c38d4d6d27c502b528fb85876972df256dbf78
Donald Brooks <brooksd079@gmail.com>
2023-06-23 21:57:54 +0300

    checks symbols and removes them from the dict

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -26,9 +26,11 @@ def get_slot_machine_spin(rows, cols, symbols):
         column = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = random.choice (current_symbols)
+            value = random.choice(current_symbols)
             current_symbols.remove(value)
-            column.append()
+            column.append(value)
+        
+        columns.append(column)
 
 #function accepts users monetary value input
 def deposit():
