ddf5ebd0c2630b9d6b7e712cee520e50e5f76b5b
Donald Brooks <brooksd079@gmail.com>
2023-06-23 21:21:23 +0300

    bet ammount and lines added to the program

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -1,7 +1,9 @@
 # Global Constant dictating the number wof lines in the slot machine
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
-
+#function accepts users monetary value input
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -17,6 +19,7 @@ def deposit():
     return amount
 
 
+#accepts the number of gambling lines 
 def get_number_of_lines():
     while True:
         lines= input("Enter the number of line to bet on (1-" + str(MAX_LINES) + ")? ")
@@ -31,9 +34,27 @@ def get_number_of_lines():
 
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"amount must ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a Number.")
+        
+    return amount 
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    total_bet =  bet * lines
+    
+    print(f"You are betting ${bet} on &{lines} lines. Total bet is equal to ${total_bet}")
+    # print(balance, lines)
 
 main()
