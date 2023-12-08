a052bf1a8324c23d8a01a1a2bea1beafe0da9e42
Donald Brooks <brooksd079@gmail.com>
2023-12-08 21:20:43 +0300

    arithmetic tests: Area of a triangle

---
+++ arithmetic/area_triangle.py
@@ -0,0 +1,9 @@
+#Area of a triangle
+
+base = float(input("Enter the base of the Triangle: "))
+height = float(input("Enter the height of the Triangle: "))
+
+#Calculate the area
+area = 0.5 * base * height
+
+print(f"The area of the Triangle is: {area}")
