ea4c6bbe7eb310c6c49cda51232bca32c8a3f6ba
Donald Brooks <brooksd079@gmail.com>
2023-12-09 23:44:26 +0300

    c to f

--- arithmetic/celsius_to_fahrenheit.py
+++ arithmetic/celsius_to_fahrenheit.py
@@ -4,4 +4,4 @@ celsius =  float(input("Enter the Temprature in Celsius: "))
 # Conversion factor: Fahrenheit = (Celsius 9/5) + 32
 Fahrenheit = (celsius * 9/5) + 32
 
-print(f"{celsius} Celsius is equivalent to {Fahrenheit} Fahrenheit")
+print(f"{celsius} Celsius is equivalent to {Fahrenheit} Fahrenheit!")
